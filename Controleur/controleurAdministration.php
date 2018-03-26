8b5fc1aa7763ccbbc04453790082a6006a75bcc5
Lestrade Antoine <LestradeAn@s2.rpn.ch>
2018-03-26 13:30:34 +0200

    Ajout Controleurs

---
+++ Controleur/controleurAdministration.php
@@ -0,0 +1,145 @@
+<?php
+
+class controleurAdministration {
+
+
+  function supprimerLog(){
+    $dateDebut = $_POST['dateDebut'];
+    $dateFin = $_POST['dateFin'];
+
+    // echo $dateDebut;
+    // echo $dateFin;
+
+    delLogConnexion($dateDebut, $dateFin);
+  }
+
+
+
+  function ajouterUtil()
+  {
+
+    $strNomUtil = protectionXSS($_POST['nomUtil']);
+    $iAdmin = 0;
+    $strMdp = protectionXSS($_POST['mdp']);
+    $strMdp = hashMotDePasse($strMdp);
+
+    if (isset($_POST['adminUtil'])) {
+      if ($_POST['adminUtil'] == "on") {
+        $iAdmin = 1;
+      }
+    }
+
+    addUtilisateur($strNomUtil, $iAdmin, $strMdp);
+  }
+
+  function modifierUtil() {
+
+    $iIDUtil;
+
+    if (isset($_POST['idUtil'])) {
+      if (protectionXSS($_POST['idUtil']) != null) {
+
+        $iIDUtil = $_POST['idUtil'];
+
+        $strNomUtil = protectionXSS($_POST['nomUtil']);
+        $iAdmin = 0;
+        $strMdp = protectionXSS($_POST['mdp']);
+
+        if ($strMdp)
+        {
+          $strMdp = hashMotDePasse($strMdp);
+
+        }
+
+
+        if (isset($_POST['adminUtil'])) {
+          if ($_POST['adminUtil'] == "on") {
+            $iAdmin = 1;
+          }
+        }
+
+        updtUtilisateur($iIDUtil, $strNomUtil, $iAdmin, $strMdp);
+      }
+    }
+
+  }
+
+  function supprimerUtil() {
+
+    $iIDUtil;
+
+
+    if (isset($_POST['idUtil'])) {
+      if (protectionXSS($_POST['idUtil']) != null) {
+        $iIDUtil = protectionXSS($_POST['idUtil']);
+
+        if (!($_SESSION['UtilisateurConnecte']->numero == $iIDUtil))
+        {
+          delUtilisateur($iIDUtil);
+        }else
+        {
+          echo "Vous ne pouvez pas supprimer votre compte utilisateur !";
+        }
+      }
+    }
+
+  }
+
+  function enregistrerParamConnexion()
+  {
+    if (isset($_POST['modeConnexion']))
+    {
+      $iModeCo = $_POST['modeConnexion'];
+
+      if ($iModeCo == 1)
+      {
+        $strMdp = protectionXSS($_POST['mdp']);
+        $strMdp = hashMotDePasse($strMdp);
+        uptModeConnexion($iModeCo, $strMdp);
+      }else{
+        uptModeConnexion($iModeCo, "");
+      }
+
+    }
+    else{
+      echo "ERREUR mode connexion";
+    }
+  }
+
+  function enregistrerParamLED()
+  {
+    if (isset($_POST['tempsImpulsion']))
+    {
+      $iTempsImpuls = $_POST['tempsImpulsion'];
+      uptTempsImpulsion($iTempsImpuls);
+    }
+    else{
+      echo "ERREUR la valeur de l'impulsion n'a pas été modifié";
+    }
+  }
+
+  function enregistrerParamSecurite()
+  {
+    if (isset($_POST['nbTentative']) && isset($_POST['tempsInterTenta']) && isset($_POST['tempsBlocage']))
+    {
+      $nbTentative = $_POST['nbTentative'];
+      $tempsInterTenta = $_POST['tempsInterTenta'];
+      $tempsBlocage = $_POST['tempsBlocage'];
+
+      if (is_null($nbTentative) == false && is_int((int)$nbTentative) && (int)$nbTentative > 0)
+      {
+        $tempsBlocage = date('H:i:s', strtotime($tempsBlocage));
+        $tempsInterTenta = date('H:i:s', strtotime($tempsInterTenta));
+
+        uptParamSecurite($nbTentative,$tempsInterTenta,$tempsBlocage);
+      }
+      else{
+        echo "ERREUR ! Entrez un nombre entier !";
+      }
+    }
+    else{
+      echo "ERREUR mode connexion";
+    }
+  }
+
+}
---
+++ Controleur/controleurPrincipal.php
@@ -0,0 +1,136 @@
+<?php
+
+class controleurPrincipal {
+  //echo "<h1>TEST CONTROLEUR !!!</h1>";
+
+  function initialiserBD() {
+    $controleurUser = new controleurUser();
+
+    $strMdp="Pass1234";
+    $strMdp= $controleurUser->hashMotDePasse($strMdp);
+    //var_dump(initDB($strMdp));
+    initDB($strMdp);
+  }
+
+  function ledImpulsion(){
+    exec('echo out > /sys/class/gpio/gpio68/direction');
+    $_SESSION['LED'] = (int)exec('cat /sys/class/gpio/gpio68/value');
+
+    $resReq = getTempsImpulsion();
+    $temps = $resReq->tempsImpulsion;
+
+    exec('echo 1 > /sys/class/gpio/gpio68/value');
+    usleep( $temps * 1000 );
+    exec('echo 0 > /sys/class/gpio/gpio68/value');
+  }
+
+  function ledOnOff(){
+    //Configuration en sortie du port
+    exec('echo out > /sys/class/gpio/gpio68/direction');
+
+    if ($_SESSION['LED']==1)
+    {
+      //Pour éteindre le port
+      exec('echo 0 > /sys/class/gpio/gpio68/value');
+    }
+    elseif ($_SESSION['LED']==0){
+      //Pour allumer le port
+      exec('echo 1 > /sys/class/gpio/gpio68/value');
+    }
+
+    $_SESSION['LED'] = (int)exec('cat /sys/class/gpio/gpio68/value');
+  }
+
+  function pagePrincipale(){
+    if (isset($_SESSION["UtilisateurConnecte"]))
+    {
+      require 'Vue/vuePrincipale.php';
+    }
+    else{
+      pageConnexion();
+    }
+  }
+
+  function pageConnexion() {
+    if (isset($_SESSION["UtilisateurConnecte"]))
+    {
+      pagePrincipale();
+    }else{
+      $pseudo = "";
+      $mdp = "";
+
+      if (isset($_POST['pseudo'])) {
+        if ((is_null($_POST['pseudo'])) == false) {
+          $pseudo = protectionXSS($_POST['pseudo']);
+          if (isset($_POST['pass'])) {
+            if ((is_null(protectionXSS($_POST['pass']))) == false) {
+              $mdp = protectionXSS($_POST['pass']);
+              require 'Vue/vueConnexion.php';
+            }
+          }
+        }
+      } else {
+        require 'Vue/vueConnexion.php';
+      }
+    }
+  }
+
+  function pageConnexionAdmin() {
+    $pseudo = "";
+    $mdp = "";
+
+    if (isset($_POST['pseudo'])) {
+      if ((is_null($_POST['pseudo'])) == false) {
+        $pseudo = protectionXSS($_POST['pseudo']);
+        if (isset($_POST['pass'])) {
+          if ((is_null(protectionXSS($_POST['pass']))) == false) {
+            $mdp = protectionXSS($_POST['pass']);
+            require 'Vue/vueConnexionAdmin.php';
+          }
+        }
+      }
+    } else {
+      require 'Vue/vueConnexionAdmin.php';
+    }
+  }
+
+  function pageEditionUtil()
+  {
+    if ($_SESSION['UtilisateurConnecte']->estAdministrateur)
+    {
+      require 'Vue/administration/vueEditionUtil.php';
+    }else{
+      pageConnexion();
+    }
+  }
+
+  function pageAdministration(){
+    if ($_SESSION['UtilisateurConnecte']->estAdministrateur)
+    {
+      require 'Vue/administration/vueAdministration.php';
+    }else{
+      pageConnexion();
+    }
+  }
+
+  function pageLogsConnexion(){
+    if ($_SESSION['UtilisateurConnecte']->estAdministrateur)
+    {
+      $listeLogs = getLogsConnexion();
+      require 'Vue/administration/vueLogsConnexion.php';
+    }else{
+      pageConnexion();
+    }
+  }
+
+    function gestionnaireErreur($errno, $errstr, $errfile, $errline){
+      echo "<img src='http://cpc.cx/lxu'><br>";
+      echo "Oups, vous n'êtiez pas sensé tombé sur ce b.. sur cette fonctionnalité qui encore en développement. Oui oui. <br><br>";
+      echo '<a href="index.php?action=pageConnexion">Je comprends. Je vais oublier tout ce qui s\'est passé et faire comme si de rien n\'était.</a>';
+      exit(1);
+
+      return true;
+
+    }
+
+  }
